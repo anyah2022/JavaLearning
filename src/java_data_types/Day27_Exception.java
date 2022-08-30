962eba8323db0ecef2901aedf1a3e071643278b3
Anya Hauk <anyah@carsonvalleyinn.com>
2022-08-29 18:51:58 -0700

    Try and Catch Exceptions

---
+++ src/java_data_types/Day27_Exception.java
@@ -0,0 +1,26 @@
+package java_data_types;
+
+import java.util.Scanner;
+
+public class Day27_Exception {
+
+	public static void main(String[] args) {
+		System.out.println("Program started");
+		Scanner sc = new Scanner(System.in);
+		try {
+			System.out.println("Enter the value of a ");
+			int a = sc.nextInt();
+			System.out.println("the value of a is " +a);
+			System.out.println("Enter the value of b ");
+			int b = sc.nextInt();
+			System.out.println("The value of b is "+b);
+			int c = a/b;
+			System.out.println("the division is " +c);
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
